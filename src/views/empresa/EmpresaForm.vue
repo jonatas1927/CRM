6e36ba146fdd2ce1dd27ee6c4dbe17699f66d34d
dionatanzimmermann <zimmermanndionatan@gmail.com>
2019-07-02 21:46:57 -0300

    Redesenhado o layout da tela empresa e feito funcionar o cadastro de uma empresa

--- src/views/empresa/EmpresaForm.vue
+++ src/views/empresa/EmpresaForm.vue
@@ -4,69 +4,115 @@
       <v-container>
         <v-layout>
           <v-flex xs12 md12>
-            <v-text-field
-              v-model="form.razaosocial"
-              :rules="razaosocialRules"
-              :counter="1000"
-              label="Razão Social"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.setor"
-              :rules="setorRules"
-              :counter="1000"
-              label="Setor"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.endereco"
-              :rules="enderecoRules"
-              :counter="1000"
-              label="Endereço"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.email"
-              :rules="emailRules"
-              :counter="1000"
-              label="E-mail"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.cnpj"
-              :rules="cnpjRules"
-              :counter="1000"
-              label="CNPJ"
-              required
-            ></v-text-field>
-            <v-text-field
+            <v-layout row>
+              <v-flex xs8 md8>
+                <v-text-field
+                  v-model="form.razaosocial"
+                  :rules="razaosocialRules"
+                  :counter="150"
+                  label="Razão Social"
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs1 md1>
+                </v-flex>
+              <v-flex xs3 md3>
+                <v-menu
+                  v-model="dataFundacao"
+                  :close-on-content-click="true"
+                  :nudge-right="40"
+                  lazy
+                  transition="scale-transition"
+                  offset-y
+                  full-width
+                  min-width="290px"
+                >
+                  <template v-slot:activator="{ on }">
+                    <v-text-field
+                      v-model="form.fundacao"
+                      label="Data Fundação"
+                      prepend-icon="event"
+                      readonly
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker lang="pt-br" v-model="form.fundacao"></v-date-picker>
+                </v-menu>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs8 md8>
+                <v-text-field
+                  v-model="form.endereco"
+                  :rules="enderecoRules"
+                  :counter="150"
+                  label="Endereço"
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs1 md1>
+                </v-flex>
+              <v-flex xs3 md3>
+                <v-text-field
+                  v-model="form.telefone"
+                  :rules="telefoneRules"
+                  :counter="1000"
+                  label="Telefone"
+                  required
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-flex xs5 md5>
+                <v-text-field
+                  v-model="form.setor"
+                  :rules="setorRules"
+                  :counter="1000"
+                  label="Setor"
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs1 md1>
+                </v-flex>
+              <v-flex xs6 md6>
+                <v-text-field
+                  v-model="form.segmento"
+                  :rules="segmentoRules"
+                  :counter="1000"
+                  label="Segmento"
+                  required
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+
+            <v-layout row>
+              <v-flex xs5 md5>
+                <v-text-field
+                  v-model="form.cnpj"
+                  :rules="cnpjRules"
+                  :counter="20"
+                  label="CNPJ"
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs1 md1></v-flex>
+              <v-flex xs6 md6>
+                <v-text-field
+                  v-model="form.email"
+                  :rules="emailRules"
+                  :counter="40"
+                  label="E-mail"
+                  required
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+            <v-textarea
               v-model="form.clientes"
               :rules="clientesRules"
               :counter="1000"
-              label="Clientes"
+              label="Principais Clientes"
               required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.telefone"
-              :rules="telefoneRules"
-              :counter="1000"
-              label="Telefone"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.fundacao"
-              :rules="fundacaoRules"
-              :counter="1000"
-              label="Data Fundação"
-              required
-            ></v-text-field>
-            <v-text-field
-              v-model="form.segmento"
-              :rules="segmentoRules"
-              :counter="1000"
-              label="Segmento"
-              required
-            ></v-text-field>
+            ></v-textarea>
           </v-flex>
           <v-flex>
             <v-btn color="success" @submit="salvar" @click="salvar">Salvar</v-btn>
@@ -93,7 +139,7 @@ export default {
     return {
       form: {
         razaosocial: "",
-        setor:  "",
+        setor: "",
         endereco: "",
         email: "",
         cnpj: "",
@@ -110,17 +156,27 @@ export default {
       Request({
         data: {
           query: `mutation {
-             createEmpresa(input: {empresa: {razaoSocial: "${this.form.razaosocial}", setor: "${this.form.setor}", endereco: "${this.form.endereco}", email: "${this.form.email}", cnpj: "${this.form.cnpj}", telefone: "${this.form.telefone}", clientes: "${this.form.clientes}", dataFundacao: "${this.form.fundacao}", segmento: "${this.form.segmento}"}}) {
+            createEmpresa(input: {empresa: {
+              razaoSocial: "${this.form.razaosocial}", 
+              setor: "${this.form.setor}", 
+              endereco: "${this.form.endereco}", 
+              email: "${this.form.email}", 
+              cnpj: "${this.form.cnpj}", 
+              telefone: "${this.form.telefone}", 
+              clientes: "${this.form.clientes}", 
+              dataFundacao: "${this.form.fundacao.toLocaleString()}", 
+              segmento: "${this.form.segmento}"
+            }}) {
                clientMutationId
-                  }
-               }`,
+            }
+          }`,
           variables: null
         }
       }).then(ret => {
         if (ret.status == 200) {
           this.mensagem = "Registro Salvo com Sucesso";
           this.snackbar = true;
-          this.$router.push({ name: "empresa" });
+          this.$router.push({ name: "empresalist" });
         } else {
           this.mensagem = "Houve um erro ao salvar o registro";
           this.snackbar = true;
